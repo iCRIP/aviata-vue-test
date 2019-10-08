d40c94333f122a2042dd11d9a1c81811db42e836
lcripl <crip@list.ru>
2019-10-08 18:34:32 +0600

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <aside class="sidebar">
+      </aside>
+      <main class="main">
+        some
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      filters: [
+        {
+          title: 'Опции тарифа',
+          name: 'rate',
+          type: 'checkbox',
+          variants: [
+            {
+              value: 1,
+              text: 'Только прямые'
+            },
+            {
+              value: 2,
+              text: 'Только с багажом'
+            },
+            {
+              value: 3,
+              text: 'Только возвратные'
+            },
+          ]
+        },
+        {
+          title: 'Авиакомпании',
+          name: 'airlines',
+          type: 'checkbox',
+          defaultVariant: {
+            value: 'all',
+            text: 'Все'
+          },
+          variants: []
+        },
+      ],
+      selectedFilters: {
+        rate: [],
+        airlines: ['all']
+      },
+      flights: [],
+    }
+  },
+
+  methods: {
+    setFiltersVariants(name, variants) {
+      const existedFilter = this.filters.find(filter => filter.name === name);
+      let newVariants = [...variants];
+      const defaultVariant = existedFilter.defaultVariant;
+      if (defaultVariant) newVariants.unshift(defaultVariant);
+
+      existedFilter.variants = newVariants;
+    },
+
+    tranformAirlines(airlines) {
+      const airlinesKeys = Object.keys(airlines);
+      const variants = airlinesKeys.map(key => ({ 
+        value: key,
+        text: airlines[key] 
+      }));
+
+      return variants;
+    },
+
+    getResults() {
+      return this.$axios
+        .get('/api/results.json')
+        .then(({ data }) => {
+          this.setFiltersVariants(
+            'airlines',
+            this.tranformAirlines(data.airlines)
+          );
+          this.flights = data.flights;
+        })
+        .catch(err => new Error(err))
+    }
+  },
+
+  created() {
+    this.getResults()
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 
+import './assets/scss/main.scss' // main shared styles
+
 Vue.config.productionTip = false
 
 new Vue({
